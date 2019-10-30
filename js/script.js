d6a8fec4554e6dc6b561e3946ec362c0cbd45769
JiaLinYang0410 <jlyang5008@gmail.com>
2019-10-30 22:35:24 +0000

    day 2

--- js/script.js
+++ js/script.js
@@ -4,6 +4,15 @@ $( "document" ).ready(function() { //this helps your jQuery to work. Write all o
         var input = $(".input").val();
         $(".output").html(input);
     });
+function word = (pig){
+let vowel = ["a" , "e" , "i" , "o" , "u"]
+let firstLetter = pig[0]
+let newWord = pig.substring(1, end)
+vowel.forEach(function(vowels){
+    if(firstLetter == vowel){
+        $(".output").html(newWord + pig[0])
+    }
+}
 
 
 
